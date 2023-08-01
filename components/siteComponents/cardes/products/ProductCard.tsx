2d89f3905b82057c51cd48b72a25d02afccebe42
moasko.dev <42101671+moasko@users.noreply.github.com>
2023-08-01 05:23:28 +0000

    new

--- components/siteComponents/cardes/products/ProductCard.tsx
+++ components/siteComponents/cardes/products/ProductCard.tsx
@@ -2,17 +2,7 @@ import { priceFormatter } from '@/lib/helpers/priceFormatter';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import {ProductProps} from "@/lib/interfaces/modelsInterfaces"
-interface Product {
-    id: number;
-    productImages: any[];
-    product_name: string;
-    price: number;
-    slug: string;
-    sale_price: number;
-    stock: number;
-    category: string;
-}
+import { ProductProps } from "@/lib/interfaces/modelsInterfaces"
 
 interface ProductCardProps {
     product_data: ProductProps;
@@ -24,6 +14,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product_data }) => {
         name,
         price,
         slug,
+        reduction
     } = product_data;
 
     const handleAddToCart = () => {
@@ -43,20 +34,20 @@ const ProductCard: React.FC<ProductCardProps> = ({ product_data }) => {
                 href={{ pathname: `/${slug}`, query: { slug: slug } }}
                 as={{ pathname: `/${slug}` }}>
                 <div className="h-[185px] w-full relative">
-                    <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill blurDataURL="./product_placeholder.png" src={images.length == 0 ? '/product_placeholder.png' : images[0].imageUrl} alt="image" />
+                    <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" fill blurDataURL="./product_placeholder.png" src={'/product_placeholder.png'} alt="image" />
                 </div>
                 <div className="prduct_name mb-2 line-clamp-2">{name}</div>
-                <div className="prix">{priceFormatter.format(price)}</div>
+                <div className="prix">{priceFormatter.format(Number(price))}</div>
                 {price != null ? <div className="prix_bare"><s>{priceFormatter.format(price)}</s></div> : null}
                 {/* {stock <= 0 ? <p className="text-red-600">Rupture de stock</p> : null} */}
             </Link>
-            {/* <button
+            <button
                 // onClick={()=>handleAddToCart()}
-                className="opacity-0 group-hover:opacity-100 w-[90%] block bg-orange-500 text-center text-white text-md uppercase mt-5 py-[8px] rounded-sm m-auto"
+                className="group-hover:opacity-90 w-[90%] block bg-orange-500 text-center text-white text-md uppercase mt-5 py-[8px] rounded-sm m-auto"
             >
                 Ajouter au panier
-            </button> */}
-            <div className="absolute top-1 right-1 bg-red-500/20 text-red-500 rounded p-1">-50%</div>
+            </button>
+            <div className="absolute top-1 right-1 bg-red-500/20 text-red-500 rounded p-1">-{reduction}%</div>
         </div>
     );
 };
