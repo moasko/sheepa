c96a24f4e46ee801348b8cf4e599e3c74d1659c0
moasko.dev <42101671+moasko@users.noreply.github.com>
2023-08-01 06:24:56 +0000

    summer

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -17,22 +17,22 @@ export default function DashboardLayout({
 }) {
   return (
     <NextUIProvider>
-     {/* header */}
-          <Nav />
-          {/* app body */}
-          <div className='w-full max-h-full max-w-full h-full flex flex-1' style={{ height: "calc(946px - 3.5rem)" }}>
-            {/* Side Bar */}
-            <SideBar />
-            <main className='flex-1 overflow-x-hidden overflow-y-auto ml-[68px] min-w-0 bg-[#f5f7f8]'>
-              <div className='container-lg h-full max-w-7xl mr-auto ml-auto'>
-                <div className="w-full h-full">
-                  {children}
-                </div>
-              </div>
-            </main>
+      {/* header */}
+      <Nav />
+      {/* app body */}
+      <div className='w-full max-h-full max-w-full h-full flex flex-1' style={{ height: "calc(946px - 3.5rem)" }}>
+        {/* Side Bar */}
+        <SideBar />
+        <main className='flex-1 overflow-x-hidden overflow-y-auto ml-[68px] min-w-0 bg-[#f5f7f8]'>
+          <div className='container-lg h-full max-w-7xl mr-auto ml-auto'>
+            <div className="w-full h-full">
+              {children}
+            </div>
           </div>
+        </main>
+      </div>
     </NextUIProvider>
-         
+
   )
 }
 
--- lib/helpers/constants.ts
+++ lib/helpers/constants.ts
@@ -1,7 +1,7 @@
 export const colors = {
     promary: "orangered"
 }
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = process.env.APP_URL
 
 export const API_BASE_URL = `${BASE_URL}/api`
 
