cff4bb8cdd70093467071800ad9c2a6d46363319
moasko.dev <42101671+moasko@users.noreply.github.com>
2023-08-01 04:39:15 +0000

    Update ProdectsGroupe.tsx

--- components/siteComponents/Listes/products/ProdectsGroupe.tsx
+++ components/siteComponents/Listes/products/ProdectsGroupe.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 import ProductCard from '../../cardes/products/ProductCard'
-import {FiChevronRight} from 'react-icons/fi'
-function ProductsGroupe({category="",title="no title", products }) {
+import { FiChevronRight } from 'react-icons/fi'
+import { ProductProps } from '@/lib/interfaces/modelsInterfaces'
+
+function ProductsGroupe({ category = "", title = "no title", products }: { category: string, title: string, products: ProductProps[] }) {
 
   return (
     <section className="row">
@@ -10,13 +12,13 @@ function ProductsGroupe({category="",title="no title", products }) {
         <div className="product-slider">
           <div className="card_title light">
             <h2 className='text-lg ml-3'>{title}</h2>
-            <Link className='flex items-center space-x-2' href={`/c/${category}`}><span>{"VOIR PLUS"}</span><FiChevronRight/></Link>
+            <Link className='flex items-center space-x-2' href={`/c/${category}`}><span>{"VOIR PLUS"}</span><FiChevronRight /></Link>
           </div>
         </div>
 
         <div className="card products_groupe">
           {
-            products?.map((product,index) => {
+            products?.map((product, index) => {
               return <ProductCard key={index} product_data={product} />
             })
           }
