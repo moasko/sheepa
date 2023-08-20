98c20d916e51a84b9c3e149c988e1d219f499e61
moasko.dev <42101671+moasko@users.noreply.github.com>
2023-08-20 20:11:06 +0000

    jop

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,11 +2,11 @@ import Footer from "@/components/siteComponents/siteLayout/Footer"
 import Header from "@/components/siteComponents/siteLayout/Header"
 import AdminToolBar from "@/components/AdminToolBar";
 import { CartProvider } from "@/contexts/CartContext";
-
+import { BASE_URL } from "@/lib/helpers/constants";
 const { SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
+  : BASE_URL;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
--- lib/helpers/constants.ts
+++ lib/helpers/constants.ts
@@ -3,7 +3,7 @@ export const colors = {
     promary: "orangered"
 }
 
-export const BASE_URL = "zangochap.vercel.app"
+export const BASE_URL = "https://zangochap.vercel.app"
 // export const BASE_URL = "http://localhost:3000"
 
 export const API_BASE_URL = `${BASE_URL}/api`
